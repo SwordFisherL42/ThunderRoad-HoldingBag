614ef404f9998869c304814ae65233425a0f8dc1
Fisher <SwordFisherL42@gmail.com>
2020-12-14 12:05:52 -0500

    [1.3.0] Update codebase for async Spawning functions introducted in u8.4

--- ItemFetch.cs
+++ ItemFetch.cs
@@ -10,18 +10,20 @@ namespace HoldingBag
     {
         protected Item item;
         protected ItemModuleFetch module;
-        protected ObjectHolder holder;
+        protected Holder holder;
         protected List<string> itemsList;
         protected List<string> parsedItemsList;
         private bool infiniteUses = false;
         private int usesRemaining = 0;
+        private bool waitingForSpawn = false;
+ 
         protected void Awake()
         {
             item = this.GetComponent<Item>();
             module = item.data.GetModule<ItemModuleFetch>();
-            holder = item.GetComponentInChildren<ObjectHolder>();
-            holder.UnSnapped += new ObjectHolder.HolderDelegate(this.OnWeaponItemRemoved);
-
+            holder = item.GetComponentInChildren<Holder>();
+            holder.UnSnapped += new Holder.HolderDelegate(this.OnWeaponItemRemoved);
+            
             //Trim list of ItemPhysic IDs by sub-type. Each ItemPhysic has its own type, defined by these enum indicies:
             //Misc = 0
             //Weapon = 1
@@ -32,7 +34,7 @@ namespace HoldingBag
             //Shield = 6
 
             //Get all ItemPhysic IDs from the chosen category. If no valid itemCategory is set, then no trimming is done and all types are valid for return
-            if (module.itemCategory >= 0 || module.itemCategory > 6)
+            if (module.itemCategory >= 0 && module.itemCategory <= 6)
             {
                 var categoryEnums = Enum.GetValues(typeof(ItemPhysic.Type));
                 ItemPhysic.Type chosenCategory = (ItemPhysic.Type)categoryEnums.GetValue(module.itemCategory);
@@ -87,10 +89,17 @@ namespace HoldingBag
             {
                 usesRemaining = module.capacity - 1;
             }
-            
-            // Spawn initial random item in the holder
-            SpawnAndSnap(GetRandomItemID(parsedItemsList), holder);
+
+            waitingForSpawn = false;
+            Debug.Log("[Fisher-HoldingBags] AWAKE HOLDING BAG: " + Time.time);
             return;
+        }
+
+        protected void Start()
+        {
+            // Spawn initial random item in the holder
+            Debug.Log("[Fisher-HoldingBags] STARTING HOLDING BAG: "  + Time.time);
+            SpawnAndSnap(GetRandomItemID(parsedItemsList), holder);
         }
 
         protected string GetRandomItemID(List<string> itemsList)
@@ -98,22 +107,41 @@ namespace HoldingBag
             return itemsList[UnityEngine.Random.Range(0, itemsList.Count)];
         }
 
-        protected void SpawnAndSnap(string spawnedItemID, ObjectHolder holder)
+        protected void SpawnAndSnap(string spawnedItemID, Holder holder)
         {
+            if (waitingForSpawn) return;
             ItemPhysic spawnedItemData = Catalog.GetData<ItemPhysic>(spawnedItemID, true);
-            //Debug.Log("[HoldingBag][Fetch][Spawn] Item Categories: " + spawnedItemData.categoryPath + ", Item Name: " + spawnedItemData.displayName);
             if (spawnedItemData == null) return;
             else
             {
-                Item thisSpawnedItem = spawnedItemData.Spawn(true);
-                if (!thisSpawnedItem.gameObject.activeInHierarchy) thisSpawnedItem.gameObject.SetActive(true);
-                holder.Snap(thisSpawnedItem);
+                waitingForSpawn = true;
+                spawnedItemData.SpawnAsync(thisSpawnedItem =>
+                {
+                    Debug.Log("[Fisher-HoldingBags] Time: " + Time.time + " Spawning weapon: " + thisSpawnedItem.name);
+                    try
+                    {
+                        waitingForSpawn = false;
+                        if (holder.HasSlotFree())
+                        {
+                            holder.Snap(thisSpawnedItem);
+                            Debug.Log("[Fisher-HoldingBags] Time: " + Time.time + " Snapped weapon: " + thisSpawnedItem.name);
+                        }
+                        else
+                        {
+                            Debug.Log("[Fisher-HoldingBags] EXCEPTION Time: " + Time.time + " NO FREE SLOT FOR: " + thisSpawnedItem.name);
+                        }
+                    }
+                    catch { Debug.Log("[Fisher-HoldingBags] EXCEPTION IN SNAPPING "); }
+                });
+                Debug.Log("[Fisher-HoldingBags] Time: " + Time.time + " Activating SpawnAndSnap: " + spawnedItemID);
                 return;
             }
         }
 
         protected void OnWeaponItemRemoved(Item interactiveObject)
         {
+            if (waitingForSpawn) return;
+
             if ((!infiniteUses) && (usesRemaining <= 0))
             {
                 holder.data.locked = true;
@@ -122,13 +150,12 @@ namespace HoldingBag
             }
             else
             {
+                Debug.Log("[Fisher-HoldingBags] Time: " + Time.time + " Activating OnWeaponItemRemoved: " + interactiveObject.data.id);
                 SpawnAndSnap(GetRandomItemID(parsedItemsList), holder);
                 usesRemaining -= 1;
                 return;
             }
-
         }
 
     }
 }
-
